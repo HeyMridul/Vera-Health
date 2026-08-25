4c69c21e963ef145d67221f40e92040c968a5af3
Mridul Bhardwaj <mridul.2005.05@gmail.com>
2026-08-25 15:45:04 +0530

    lib

---
+++ client/src/lib/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: '/api'
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('auth_token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export function getErrorMessage(err) {
+  return err?.response?.data?.error || 'Something went wrong. Please try again.';
+}
+
+export default api;
